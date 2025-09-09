54b1abbd872983fdb3b306fd00803f042a10825d
mendeszyy <silva.mendes.paola@escola.pr.gov.br>
2025-09-09 15:21:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
